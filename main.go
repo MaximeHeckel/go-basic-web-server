7ab6764dbc8f158cf0e8971bf550442eaaa38ddc
Maxime Heckel <heckelmaxime@gmail.com>
2015-01-25 21:29:17 +0100

    change port

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
-import(
-  "fmt"
-  "net/http"
-  "runtime"
+import (
+	"fmt"
+	"net/http"
+	"runtime"
 )
 
-func indexHandler( w http.ResponseWriter, r *http.Request){
-  fmt.Fprintf(w, "<h1>hello, world</h1>\nI'm running on %s with an %s CPU ", runtime.GOOS,runtime.GOARCH)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>hello, world</h1>\nI'm running on %s with an %s CPU ", runtime.GOOS, runtime.GOARCH)
 }
 
-func main(){
-  http.HandleFunc("/", indexHandler)
-  http.ListenAndServe(":8080",nil)
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":80", nil)
+  fmt.Println("running")
 }
