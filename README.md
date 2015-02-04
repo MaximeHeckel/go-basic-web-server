f63953eb30cac59eca1cd51cfca97d726c11a0d8
Maxime Heckel <heckelmaxime@gmail.com>
2015-02-04 19:13:18 +0100

    more complex example using MongoDB

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Go-basic-web-server
 ===================
 
 A basic web server written in Golang for educational purposes
+This app uses mgo as MongoDB driver and the standard http library to handle requests
--- main.go
+++ main.go
@@ -2,16 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"runtime"
+
+	"gopkg.in/mgo.v2/bson"
+
+	"gopkg.in/mgo.v2"
 )
 
+type Person struct {
+	Name string
+	Type string
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>hello, world</h1>\nI'm running on %s with an %s CPU ", runtime.GOOS, runtime.GOARCH)
+	fmt.Fprintf(w, "<h1>hello, world</h1>\nI'm running on %s with an %s CPU the name of this program is : %s", runtime.GOOS, runtime.GOARCH, os.Getenv("NAME"))
 }
 
 func main() {
+	session, err := mgo.Dial(os.Getenv("MONGO"))
+
+	if err == nil {
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, true)
+		c := session.DB("quicksell").C("people")
+		err = c.Insert(&Person{"Golang", "Awesome"})
+		if err != nil {
+			log.Fatal(err)
+		}
+		result := Person{}
+
+		err = c.Find(bson.M{"name": "Golang"}).One(&result)
+		fmt.Println("Golang is: ", result.Type)
+	}
 	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":80", nil)
-  fmt.Println("running")
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("running")
 }
